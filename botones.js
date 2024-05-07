6c1331e45bafec90b60d75c8c0193876b61027ba
Cristobal J. Martinez Lero <julian.martinezlero@gmail.com>
2024-05-07 15:44:34 -0400

    herramientas para su tarea

--- botones.js
+++ botones.js
@@ -1,24 +1,19 @@
 function procesaA() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      numero = numero + 1
-      console.log("a", numero)
-      resolve(true)
-    }, 5000)
-  })
-
-
+      numero = numero + 1;
+      console.log("a", numero);
+      resolve(true);
+    }, 5000);
+  });
 }
 
 function procesaB() {
-  console.log("b", numero)
+  console.log("b", numero);
 }
 
-
-var numero = 10000
-
-
+var numero = 10000;
 
 procesaA().then(() => {
-  procesaB()
-})
+  procesaB();
+});
